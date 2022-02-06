f4f41aab2561df813df1a2163f22323b923ee58d
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-02-06 13:24:32 +0100

    Deleting console log

--- src/services/getGifs.jsx
+++ src/services/getGifs.jsx
@@ -1,5 +1,4 @@
 function getGifs({keyword = 'rick'} = {}) {
-    console.log(process.env.REACT_APP_API_KEY)
     const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}=${keyword}&limit=10&offset=0&rating=g&lang=es`
 
     return (
