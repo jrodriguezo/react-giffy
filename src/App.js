129c3283ac4a18c701000e718bea45f89326eaf3
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-02-06 11:37:59 +0100

    Cleaning unused variables and styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [contador, setContador] = useState(0)
 
   return (
     <div className="App">
-      <h1>React - giffy</h1>
+      <div className="App-content">
+        <h1>React - giffy</h1>
+      </div>
     </div>
   );
 }
